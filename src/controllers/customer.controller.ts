f7ba857fab81b214edb2fb94fa372485cd19067f
Wardner Lara <51765669+Wardner@users.noreply.github.com>
2020-04-18 12:57:19 -0400

    Update customer.controller.ts

--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -2,16 +2,6 @@ import {Request, Response} from 'express';
 import { Customer } from '../entities/customer.entity';
 import { getRepository } from 'typeorm';
 
-// export const getCustomers = async (req: Request, res: Response): Promise<Response> => {
-//   //  const customers = await getRepository(Customer).find({isActive: true});
-//     const customers = await getRepository(Customer).find();
-//     if(customers.length > 0){
-//         return res.json(customers);
-//     }
-
-//     return res.status(400).json({msg: 'Customers not found'});
-// }
-
 export const getCustomers = async (req: Request, res: Response): Promise<Response> => {
     const customers = await getRepository(Customer)
         .createQueryBuilder('customer')
@@ -23,15 +13,6 @@ export const getCustomers = async (req: Request, res: Response): Promise<Respons
 
     return res.status(400).json({msg: 'Customers Not Found'});
 }
-
-// export const getCustomer = async (req: Request, res: Response): Promise<Response> => {
-//     const customer = await getRepository(Customer).findOne(req.params.dni);
-//     if(!customer){
-//         return res.status(400).json({msg: 'Customer not found'});
-//     }
-    
-//     return res.json(customer);
-// }
 
 export const getCustomer = async (req: Request, res: Response): Promise<Response> => {
     const customer = await getRepository(Customer)
@@ -64,13 +45,6 @@ export const updateCustomer = async (req: Request, res: Response): Promise<Respo
 }
 
 export const deleteCustomer = async (req: Request, res: Response): Promise<Response> => {
-    // const customer = await getRepository(Customer).findOne(req.params.dni);
-    // if(customer){
-    //     getRepository(Customer).merge(customer, {isActive: false});
-    //     await getRepository(Customer).save(customer);
-    //     return res.json({msg: 'Customer Eliminated'});
-    // }
-    // return res.status(400).json({msg: 'Customer Not Found'});
     const customer = await getRepository(Customer).delete(req.params);
     return res.json({msg: 'Customer Eliminated'});
 }
